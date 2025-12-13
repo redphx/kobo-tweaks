redphx/kobo-tweaks

commit 4757358d1a5b771747e4deb30738740b9e9ca96c

fix: fix memory safety and undefined behaviour in ReadingView hooks (#8)
* fix: fix memory safety and undefined behaviour in ReadingView hooks

* Fix incorrect return type for constructor.
* Fix ReadingSettings memory leak.
* Fix use-after-free on widgets in certain situations.
* Fix incorrect declaration for insertWidgets.
* Use signals to notify widgets.
* Simplify ReadingView hooks.

* chore: stop logging in notifyPageChanged()

---------

Co-authored-by: redphx <96280+redphx@users.noreply.github.com>

src/settings/settings.cc
@@ -1,10 +1,7 @@
 #include "settings.h"
 #include "../utils.h"
 
-TweaksSettings::TweaksSettings() : qSettings(DATA_DIR "/settings.ini", QSettings::IniFormat) {
-    load();
-    sync();
-}
+TweaksSettings::TweaksSettings() : qSettings(DATA_DIR "/settings.ini", QSettings::IniFormat) {}
 
 QString validateImage(const QString& path) {
     QPixmap pix;

src/common.cc
@@ -1,6 +1,6 @@
 #include "common.h"
 
-ReadingView* (*ReadingView_constructor)(ReadingView* self) = nullptr;
+void (*ReadingView_constructor)(ReadingView* self) = nullptr;
 void (*ReadingFooter_setFooterMargin)(QWidget* self, int margin) = nullptr;
 void (*ReadingView_pageChanged)(ReadingView* self, int pageIndex) = nullptr;
 void (*SearchAutoCompleteController_handleSpecialCommands)(SearchAutoCompleteController* self, const QString& command) = nullptr;

src/common.h
@@ -2,6 +2,8 @@
 
 #include <QWidget>
 
+#include <NickelHook.h>
+
 #ifndef DATA_DIR
     #define DATA_DIR "/mnt/onboard/.adds/tweaks"
 #endif
@@ -21,6 +23,7 @@
 enum class ReadingFooterPosition { Header, Footer };
 
 typedef QWidget ReadingView;
+typedef QWidget GestureReceivingContainer;
 typedef QWidget ReadingFooter;
 typedef QObject HardwareInterface;
 typedef void SearchAutoCompleteController;
@@ -28,9 +31,8 @@ typedef void SearchAutoCompleteController;
 using GetBatteryLevelFn = int(*)(HardwareInterface*);
 using ChargingStateFn = uint(*)(HardwareInterface*);
 
-extern ReadingView* (*ReadingView_constructor)(ReadingView* self);
+extern void (*ReadingView_constructor)(ReadingView* self);
 extern void (*ReadingFooter_setFooterMargin)(QWidget* self, int margin);
-extern void (*ReadingView_pageChanged)(ReadingView* self, int pageIndex);
 extern void (*SearchAutoCompleteController_handleSpecialCommands)(SearchAutoCompleteController* self, const QString& command);
 extern void (*ConfirmationDialogFactory_showOKDialog)(QString const& title, QString const& body);
 extern QWidget* (*DogEarDelegate_constructor)(QWidget* self, QWidget* parent, const QString& image);

src/hooks/reading_view.cc
@@ -1,38 +1,63 @@
 #include "reading_view.h"
 
 namespace ReadingViewHook {
-    TweaksSettings* settings;
-    const TweaksReadingSettings* readingSettings = nullptr;
-    bool isDarkMode = false;
-    WidgetRefs refs;
+    static TweaksSettings settings;
+    static bool isDarkMode = false;
 
-    class GestureContainerEventFilter : public BaseEventFilter {
-        bool eventFilter(QObject* obj, QEvent* event) override {
-            QEvent::Type type = event->type();
-            if (type == QEvent::DynamicPropertyChange) {
-                auto *e = static_cast<QDynamicPropertyChangeEvent *>(event);
-
-                const QByteArray name = e->propertyName();
-                if (name == QStringLiteral("darkMode")) {
-                    isDarkMode = obj->property(name.constData()).toBool();
-                    _updateWidgets();
-                }
-            }
-
-            return BaseEventFilter::eventFilter(obj, event);
+    PageChangedAdapter::PageChangedAdapter(ReadingView *parent) : QObject(parent) {
+        if (!QObject::connect(parent, SIGNAL(pageChanged(int)), this, SLOT(notifyPageChanged()), Qt::UniqueConnection)) {
+            nh_log("failed to connect _ZN11ReadingView11pageChangedEi");
         }
-    };
+    }
 
-    void _insertWidgets(ReadingFooter* footer, QString qss, WidgetTypeEnum leftType, WidgetTypeEnum rightType, bool isDarkMode) {
+    void PageChangedAdapter::notifyPageChanged() {
+        // nh_log("page changed");
+        pageChanged();
+    }
+
+    DarkModeAdapter::DarkModeAdapter(GestureReceivingContainer *parent, ReadingView *view) : QObject(parent) {
+        if (!QObject::connect(view, SIGNAL(darkModeChangedSignal()), this, SLOT(notifyDarkModeChanged()), Qt::UniqueConnection)) {
+            nh_log("failed to connect _ZN11ReadingView21darkModeChangedSignalEv");
+        }
+    }
+
+    bool DarkModeAdapter::getDarkMode() {
+        // the property is set by ReadingView by ReadingView::darkModeChanged
+        // (which calls darkModeChangedSignal afterwards) on the
+        // GestureReceivingContainer set up in Ui_ReadingView::setupUi called by
+        // the constructor
+
+        // if this is no longer viable, we could also call
+        // ReadingSettings::getDarkMode, but this won't handle the cases where
+        // ReadingView doesn't support dark mode for the current format (e.g.,
+        // audiobooks)
+
+        // it's also stored as a field on ReadingView, but that would require
+        // hardcoding offsets
+
+        auto prop = parent()->property("darkMode");
+        if (!prop.isValid()) {
+            nh_log("darkMode property not set on GestureReceivingContainer");
+            return false;
+        }
+        return prop.toBool();
+    }
+
+    void DarkModeAdapter::notifyDarkModeChanged() {
+        auto dark = getDarkMode();
+        nh_log("dark mode changed (%s)", dark ? "dark" : "light");
+        darkModeChanged(dark);
+    }
+
+
+    static void insertWidgets(PageChangedAdapter *pageChangedAdapter, DarkModeAdapter *darkModeAdapter, ReadingFooter* footer, QString qss, WidgetTypeEnum leftType, WidgetTypeEnum rightType) {
         bool hasLeft = false;
+        auto readingSettings = settings.getReadingSettings();
 
         HardwareInterface* hardwareInterface = HardwareFactory_sharedInstance();
 
         footer->setStyleSheet(qss);
         footer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred); // stretch
-
-        TwClockWidgetConfig clockConfig {};
-        TwBatteryWidgetConfig batteryConfig {};
 
         for (auto p : {leftType, rightType}) {
             bool isLeft = p == leftType;
@@ -40,21 +65,30 @@ namespace ReadingViewHook {
 
             switch (p) {
                 case WidgetTypeEnum::Clock:
-                    clockConfig.isLeft = isLeft;
-                    clockConfig.is24hFormat = readingSettings->widgetClock24hFormat;
+                    {
+                        TwClockWidgetConfig config {};
+                        config.isLeft = isLeft;
+                        config.is24hFormat = readingSettings.widgetClock24hFormat;
 
-                    widget = new TwClockWidget(clockConfig);
-                    refs.clockWidget = qobject_cast<TwClockWidget*>(widget);
+                        auto clock = new TwClockWidget(config);
+                        QObject::connect(pageChangedAdapter, &PageChangedAdapter::pageChanged, clock, &TwClockWidget::updateTime, Qt::UniqueConnection);
+                        widget = clock;
+                    }
                     break;
                 case WidgetTypeEnum::Battery:
-                    batteryConfig.isLeft = isLeft;
-                    batteryConfig.isDarkMode = isDarkMode;
-                    batteryConfig.defaultStyle = readingSettings->widgetBatteryStyle;
-                    batteryConfig.chargingStyle = readingSettings->widgetBatteryStyleCharging;
-                    batteryConfig.showWhenBelow = readingSettings->widgetBatteryShowWhenBelow;
+                    {
+                        TwBatteryWidgetConfig config {};
+                        config.isDarkMode = darkModeAdapter->getDarkMode();
+                        config.isLeft = isLeft;
+                        config.defaultStyle = readingSettings.widgetBatteryStyle;
+                        config.chargingStyle = readingSettings.widgetBatteryStyleCharging;
+                        config.showWhenBelow = readingSettings.widgetBatteryShowWhenBelow;
 
-                    widget = new TwBatteryWidget(batteryConfig, hardwareInterface);
-                    refs.batteryWidget = qobject_cast<TwBatteryWidget*>(widget);
+                        auto battery = new TwBatteryWidget(config, hardwareInterface);
+                        QObject::connect(darkModeAdapter, &DarkModeAdapter::darkModeChanged, battery, &TwBatteryWidget::setDarkMode, Qt::UniqueConnection);
+                        QObject::connect(pageChangedAdapter, &PageChangedAdapter::pageChanged, battery, &TwBatteryWidget::updateLevel, Qt::UniqueConnection);
+                        widget = battery;
+                    }
                     break;
                 default:
                     continue;
@@ -83,7 +117,7 @@ namespace ReadingViewHook {
                 }
             }
 
-            // Set TimeLabel width to the original margin value
+            // Set widget's width to the original margin value
             int layoutMargin = footer->property("twks_margin").toInt();
             widget->setFixedWidth(layoutMargin);
             widget->setContentsMargins(20, 0, 20, 0);
@@ -98,30 +132,20 @@ namespace ReadingViewHook {
         }
     }
 
-    void _updateWidgets() {
-        if (refs.clockWidget) {
-            refs.clockWidget->updateTime();
-        }
+    void constructor(ReadingView* view) {
+        // Must parse settings before constructor since other widgets use them
+        settings.load();
+        settings.sync();
 
-        if (refs.batteryWidget) {
-            refs.batteryWidget->updateLevel(isDarkMode);
-        }
-    }
+        ReadingView_constructor(view);
 
-    ReadingView* constructor(ReadingView* self) {
-        // Must parse settings before constructor
-        settings = new TweaksSettings();
-        readingSettings = &settings->getReadingSettings();
+        // MUST NOT KEEP REFS TO THE WIDGETS, AS WE DON'T CONTROL THE LIFETIME
 
-        auto view = ReadingView_constructor(self);
-
-        refs = WidgetRefs{};
-        refs.readingView = view;
-
-        QWidget* gestureContainer = view->findChild<QWidget*>(QStringLiteral("gestureContainer"), Qt::FindDirectChildrenOnly);
+        // Note: created and passed to grabGestures in the constructor, most events are passed to it
+        QWidget* gestureContainer = view->findChild<GestureReceivingContainer*>(QStringLiteral("gestureContainer"), Qt::FindDirectChildrenOnly);
         if (!gestureContainer) {
             nh_log("could not find \"gestureContainer\"");
-            return view;
+            return;
         }
 
         // Find "header"
@@ -129,40 +153,35 @@ namespace ReadingViewHook {
         QWidget* footer = gestureContainer->findChild<ReadingFooter*>(QStringLiteral("footer"), Qt::FindDirectChildrenOnly);
         if (!header || !footer) {
             nh_log("could not find \"header/footer\"");
-            return view;
+            return;
         }
 
-        refs.gestureContainer = gestureContainer;
-        refs.header = header;
-        refs.footer = footer;
+        // These adapters abstract the logic and ensure that the update methods on the widgets aren't called after either the widget or the ReadingView has been destroyed
+        auto pageChangedAdapter = new PageChangedAdapter(view);
+        auto darkModeAdapter = new DarkModeAdapter(gestureContainer, view);
 
-        // Detect Dark mode
-        isDarkMode = gestureContainer->property("darkMode").toBool();
+        auto readingSettings = settings.getReadingSettings();
+
+        isDarkMode = darkModeAdapter->getDarkMode();
+
+        QObject::connect(darkModeAdapter, &DarkModeAdapter::darkModeChanged, [](bool dark) {
+            isDarkMode = dark;
+        });
 
         QString readingFooterQss = Qss::getContent(QStringLiteral(":/qss/ReadingFooter.qss"));
         QString patchedQss = Qss::copySelectors(readingFooterQss, QStringLiteral("#caption"), QStringList() << QStringLiteral("#twks_clock #label") << QStringLiteral("#twks_battery #label"));
 
-        if (readingSettings->headerFooterHeightScale < 100) {
-            patchedQss = Patch::ReadingView::scaleHeaderFooterHeight(patchedQss, readingSettings->headerFooterHeightScale);
+        if (readingSettings.headerFooterHeightScale < 100) {
+            patchedQss = Patch::ReadingView::scaleHeaderFooterHeight(patchedQss, readingSettings.headerFooterHeightScale);
         }
 
-        _insertWidgets(refs.header, patchedQss, readingSettings->widgetHeaderLeft, readingSettings->widgetHeaderRight, isDarkMode);
-        _insertWidgets(refs.footer, patchedQss, readingSettings->widgetFooterLeft, readingSettings->widgetFooterRight, isDarkMode);
-
-        // Register event filter for "darkMode" property
-        gestureContainer->installEventFilter(new GestureContainerEventFilter());
-
-        return view;
-    }
-
-    void pageChanged(ReadingView* self, int page) {
-        // nh_log("hook_ReadingView_pageChanged");
-
-        _updateWidgets();
-        ReadingView_pageChanged(self, page);
+        insertWidgets(pageChangedAdapter, darkModeAdapter, header, patchedQss, readingSettings.widgetHeaderLeft, readingSettings.widgetHeaderRight);
+        insertWidgets(pageChangedAdapter, darkModeAdapter, footer, patchedQss, readingSettings.widgetFooterLeft, readingSettings.widgetFooterRight);
     }
 
     void setFooterMargin(QWidget* self, int margin) {
+        auto readingSettings = settings.getReadingSettings();
+
         // Store the original margin to "twks_margin"
         self->setProperty("twks_margin", margin);
 
@@ -172,11 +191,11 @@ namespace ReadingViewHook {
         WidgetTypeEnum leftType = WidgetTypeEnum::Invalid;
         WidgetTypeEnum rightType = WidgetTypeEnum::Invalid;
         if (widgetName == QStringLiteral("header")) {
-            leftType = readingSettings->widgetHeaderLeft;
-            rightType = readingSettings->widgetHeaderRight;
+            leftType = readingSettings.widgetHeaderLeft;
+            rightType = readingSettings.widgetHeaderRight;
         } else {
-            leftType = readingSettings->widgetFooterLeft;
-            rightType = readingSettings->widgetFooterRight;
+            leftType = readingSettings.widgetFooterLeft;
+            rightType = readingSettings.widgetFooterRight;
         }
 
         int leftMargin = leftType != WidgetTypeEnum::Invalid ? 0 : margin;
@@ -186,7 +205,7 @@ namespace ReadingViewHook {
 
     namespace DogEarDelegate {
         QWidget* constructor(QWidget* self, QWidget* parent, const QString& orgImgPath) {
-            QString imgPath = settings->getReadingBookmarkImage(isDarkMode);
+            QString imgPath = settings.getReadingBookmarkImage(isDarkMode);
             if (imgPath.isEmpty()) {
                 imgPath = orgImgPath;
             }

src/hooks/reading_view.h
@@ -9,21 +9,35 @@
 
 
 namespace ReadingViewHook {
-    struct WidgetRefs {
-        ReadingView* readingView = nullptr;
-        QWidget* gestureContainer = nullptr;
-        ReadingFooter* header = nullptr;
-        ReadingFooter* footer = nullptr;
+    class PageChangedAdapter : public QObject {
+        Q_OBJECT
 
-        TwClockWidget* clockWidget = nullptr;
-        TwBatteryWidget* batteryWidget = nullptr;
+    public:
+        explicit PageChangedAdapter(ReadingView *parent);
+
+    private slots:
+        void notifyPageChanged();
+
+    signals:
+        void pageChanged();
     };
 
-    void _insertWidgets(ReadingFooter footer, WidgetTypeEnum leftType, WidgetTypeEnum rightType);
-    void _updateWidgets();
+    class DarkModeAdapter : public QObject {
+        Q_OBJECT
 
-    ReadingView* constructor(ReadingView* self);
-    void pageChanged(ReadingView* self, int page);
+    public:
+        explicit DarkModeAdapter(GestureReceivingContainer *parent, ReadingView *view);
+
+        bool getDarkMode();
+
+    private slots:
+        void notifyDarkModeChanged();
+
+    signals:
+        void darkModeChanged(bool dark);
+    };
+
+    void constructor(ReadingView* self);
     void setFooterMargin(QWidget* self, int margin);
 
     namespace DogEarDelegate {

src/settings/settings.h
@@ -135,11 +135,11 @@ class TweaksSettings {
 
         TweaksSettings();
         void sync();
+        void load();
 
     private:
         QSettings qSettings;
         TweaksReadingSettings readingSettings;
 
-        void load();
         void loadReadingSettings();
 };

src/tweaks.cc
@@ -1,4 +1,3 @@
-#include <NickelHook.h>
 #include "tweaks.h"
 #include "common.h"
 #include "debug_utils.h"
@@ -43,14 +42,6 @@ struct nh_hook TweaksHook[] = {
         .sym_new  = "hook_ReadingFooter_setFooterMargin",
         .lib      = "libnickel.so.1.0.0",
         .out      = nh_symoutptr(ReadingFooter_setFooterMargin),
-        .desc     = "ReadingView",
-        .optional = true,
-    },
-    {
-        .sym      = "_ZN11ReadingView11pageChangedEi", 
-        .sym_new  = "hook_ReadingView_pageChanged",
-        .lib      = "libnickel.so.1.0.0",
-        .out      = nh_symoutptr(ReadingView_pageChanged),
         .desc     = "ReadingView",
         .optional = true,
     },
@@ -135,19 +126,13 @@ NickelHook(
 
 // HOOKS
 extern "C" __attribute__((visibility("default")))
-ReadingView* hook_ReadingView_constructor(ReadingView* self) {
-    return ReadingViewHook::constructor(self);
+void hook_ReadingView_constructor(ReadingView* self) {
+    ReadingViewHook::constructor(self);
 }
 
 extern "C" __attribute__((visibility("default")))
 void hook_ReadingFooter_setFooterMargin(QWidget* self, int margin) {
     ReadingViewHook::setFooterMargin(self, margin);
-}
-
-
-extern "C" __attribute__((visibility("default")))
-void hook_ReadingView_pageChanged(ReadingView* self, int page_index) {
-    ReadingViewHook::pageChanged(self, page_index);
 }
 
 extern "C" __attribute__((visibility("default")))

src/widgets/battery_widget.h
@@ -10,7 +10,6 @@
 #include <QLabel>
 #include <QPixmap>
 #include <QEvent>
-#include <NickelHook.h>
 
 struct TwBatteryWidgetConfig {
     bool isDarkMode = false;
@@ -76,17 +75,84 @@ public:
         levelLabel->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
         // levelLabel->setStyleSheet("border: 1px solid black;");
 
-        updateLevel(currentDarkMode);
+        updateLevel();
         show();
+    }
+
+    void setDarkMode(bool dark) {
+        currentDarkMode = dark;
+        updateDisplay();
+    }
+
+    void updateLevel() {
+        pollBatteryLevel();
+        pollChargingState();
+        updateDisplay();
     }
 
 protected:
     bool event(QEvent* e) override {
         if (e->type() == QEvent::Show) {
-            updateLevel(currentDarkMode);
+            updateLevel();
         }
 
         return QWidget::event(e);
+    }
+
+private:
+    void updateDisplay() {
+        // Hide layout when level is above the threshold
+        if (currentChargingState == ChargingState::Unplugged && currentBatteryLevel > showWhenBelow) {
+            hide();
+            return;
+        }
+
+        show();
+
+        currentStyle = currentChargingState == ChargingState::Unplugged ? defaultStyle : chargingStyle;
+        if (currentStyle != lastStyle) {
+            refreshStyle();
+            lastStyle = currentStyle;
+        }
+
+        if (lastDarkMode == currentDarkMode && lastBatteryLevel == currentBatteryLevel && lastChargingState == currentChargingState) {
+            return;
+        }
+
+        lastBatteryLevel = currentBatteryLevel;
+        lastChargingState = currentChargingState;
+        lastDarkMode = currentDarkMode;
+
+        int pixmapIndex = getPixmapIndex(currentBatteryLevel);
+        QString iconPath;
+        switch (currentChargingState) {
+            case ChargingState::Charged:
+                iconPath = QStringLiteral(":/kobo_tweaks/images/battery_charged.png");
+                currentBatteryLevel = 100;
+                break;
+            case ChargingState::Charging:
+                iconPath = QStringLiteral(":/kobo_tweaks/images/battery_charging_%1.png").arg(pixmapIndex);
+                break;
+            default:
+                iconPath = QStringLiteral(":/kobo_tweaks/images/battery_%1.png").arg(pixmapIndex);
+                break;
+        }
+
+        if (currentDarkMode) {
+            iconPath = Utils::appendFileName(iconPath, QStringLiteral("_dark"));
+        }
+
+        QPixmap icon(iconPath);
+        iconLabel->setPixmap(icon);
+        iconLabel->setFixedSize(icon.size());
+
+        QString text;
+        if (currentBatteryLevel > 0) {
+            text = QStringLiteral("%1\%").arg(currentBatteryLevel);
+        } else {
+            text = QStringLiteral("??%");
+        }
+        levelLabel->setText(text);
     }
 
     void refreshStyle() {
@@ -157,65 +223,5 @@ protected:
 
     int getPixmapIndex(int batteryLevel) {
         return qBound(1, (batteryLevel + 9) / 10, 10);
-    }
-
-public slots:
-    void updateLevel(bool dark) {
-        pollBatteryLevel();
-        pollChargingState();
-
-        // Hide layout when level is above the threshold
-        if (currentChargingState == ChargingState::Unplugged && currentBatteryLevel > showWhenBelow) {
-            hide();
-            return;
-        }
-
-        show();
-
-        currentDarkMode = dark;
-        currentStyle = currentChargingState == ChargingState::Unplugged ? defaultStyle : chargingStyle;
-        if (currentStyle != lastStyle) {
-            refreshStyle();
-            lastStyle = currentStyle;
-        }
-
-        if (lastDarkMode == currentDarkMode && lastBatteryLevel == currentBatteryLevel && lastChargingState == currentChargingState) {
-            return;
-        }
-
-        lastBatteryLevel = currentBatteryLevel;
-        lastChargingState = currentChargingState;
-        lastDarkMode = currentDarkMode;
-
-        int pixmapIndex = getPixmapIndex(currentBatteryLevel);
-        QString iconPath;
-        switch (currentChargingState) {
-            case ChargingState::Charged:
-                iconPath = QStringLiteral(":/kobo_tweaks/images/battery_charged.png");
-                currentBatteryLevel = 100;
-                break;
-            case ChargingState::Charging:
-                iconPath = QStringLiteral(":/kobo_tweaks/images/battery_charging_%1.png").arg(pixmapIndex);
-                break;
-            default:
-                iconPath = QStringLiteral(":/kobo_tweaks/images/battery_%1.png").arg(pixmapIndex);
-                break;
-        }
-
-        if (currentDarkMode) {
-            iconPath = Utils::appendFileName(iconPath, QStringLiteral("_dark"));
-        }
-
-        QPixmap icon(iconPath);
-        iconLabel->setPixmap(icon);
-        iconLabel->setFixedSize(icon.size());
-
-        QString text;
-        if (currentBatteryLevel > 0) {
-            text = QStringLiteral("%1\%").arg(currentBatteryLevel);
-        } else {
-            text = QStringLiteral("??%");
-        }
-        levelLabel->setText(text);
     }
 };

commit 3b7f644ea76cb118822627c4a5464c98a178257c

fix: ensure all settings are reloaded every time
fixes a regression in 4757358d1a5b771747e4deb30738740b9e9ca96c

src/settings/settings.cc
@@ -67,6 +67,7 @@ void TweaksSettings::loadReadingSettings() {
 }
 
 void TweaksSettings::load() {
+    qSettings.sync();
     loadReadingSettings();
 }
 