844aafef441ef3715e9733e0b7b881146f955ffb
Patrick Gaskin <patrick@pgaskin.net>
2025-12-13 00:26:05 -0500

    refactor: compile resources using NickelHook Makefile
    also add clangd to gitignore and use built-in clean option

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 # Remove old files
-rm -f KoboRoot.tgz libtweaks.so
-find ./src -type f \( -name '*.o' -o -name '*.moc' \) -delete
-
-# Build resources
-set -a
-source .env
-set +a
-"$RCC_PATH" src/resources.qrc -o src/resources.qrc.cpp
+docker run --volume="$PWD:$PWD" --user="$(id -u):$(id -g)" --workdir="$PWD" --env=HOME --entrypoint=make --rm -it ghcr.io/pgaskin/nickeltc:1.0 clean
 
 # Build plugin
 docker run --volume="$PWD:$PWD" --user="$(id -u):$(id -g)" --workdir="$PWD" --env=HOME --entrypoint=make --rm -it ghcr.io/pgaskin/nickeltc:1.0 all koboroot
