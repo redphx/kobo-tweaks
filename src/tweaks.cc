090f33cad8bf817ec256b8a87d6d93589c16b5db
redphx <96280+redphx@users.noreply.github.com>
2026-02-01 16:15:24 +0700

    perf: optimize popup rendering

--- src/tweaks.cc
+++ src/tweaks.cc
@@ -376,29 +376,27 @@ QPointer<QWidget> globalPopupKeyboardController = nullptr;
 
 extern "C" __attribute__((visibility("default")))
 void hook_SearchKeyboardController_popupKeyboard(SearchKeyboardController* self, VirtualKey* key, QVector<KeyboardLayoutRow> rows) {
+    // Kobo only renders the first row of keys correctly (the rest are inserted into PopupKeyboard without wrapping inside a layout)
+    // To fix this, we only let Kobo render the first row. But we also put other rows as empty lists so the popup is prepared correctly
     const QString keyboardLocale = "vi-VN";
 
     QVector<KeyboardLayoutRow> additionalRows;
-    bool customPopupKeys = false;
-    QString label = *reinterpret_cast<QString*>(VirtualKey_text(key));
+    const QString& label = *reinterpret_cast<QString*>(VirtualKey_text(key));
 
     // Get custom popup keys
     if (KEYBOARD_MAP.contains(keyboardLocale)) {
-        const auto& keyboardData = KEYBOARD_MAP[keyboardLocale];
-        if (keyboardData.popupMap.contains(label)) {
-            customPopupKeys = true;
-
+        const auto& popupMap = KEYBOARD_MAP[keyboardLocale].popupMap;
+        if (popupMap.contains(label)) {
             // Remove old keys
             rows.clear();
 
-            // Only setup keys in the first row
-            auto popupRows = keyboardData.popupMap[label];
-
             int i = -1;
+            auto popupRows = popupMap.value(label);
             for (const auto& popupRow : popupRows) {
                 ++i;
                 KeyboardLayoutRow keysRow;
 
+                // keyId (0xffff0000) can be the same for every keys, as long as it's not from special keys
                 for (const char* keyLabel : popupRow) {
                     keysRow.keys.append(SearchKeyboardController_newKey(self, keyLabel,  0xffff0000, 10));
                 }
@@ -406,7 +404,7 @@ void hook_SearchKeyboardController_popupKeyboard(SearchKeyboardController* self,
                 if (i == 0) {
                     rows.append(keysRow);
                 } else {
-                    // Add empty row so Kobo increases the popup's height
+                    // Add empty row so Kobo increases NickelTouchMenu's height
                     rows.append(KeyboardLayoutRow());
                     additionalRows.append(keysRow);
                 }
@@ -416,47 +414,48 @@ void hook_SearchKeyboardController_popupKeyboard(SearchKeyboardController* self,
 
     // Let Kobo setup the keys
     SearchKeyboardController_popupKeyboard(self, key, rows);
-    if (!customPopupKeys) {
+
+    if (additionalRows.isEmpty() || !globalPopupKeyboardController || !PopupKeyboardController_menu) {
         globalPopupKeyboardController = nullptr;
         return;
     }
 
-    if (additionalRows.isEmpty()) {
-        return;
-    }
-
-    if (!globalPopupKeyboardController || !PopupKeyboardController_menu) {
-        return;
-    }
-
     NickelTouchMenu* menu = PopupKeyboardController_menu(globalPopupKeyboardController);
+    globalPopupKeyboardController = nullptr;
+
     PopupKeyboard* popupKeyboard = menu->findChild<PopupKeyboard*>(QString());
     if (!popupKeyboard) {
         return;
     }
 
+    int rowsCount = additionalRows.size();
     QSize* keySize = VirtualKeyboard_keySize(popupKeyboard);
     QVBoxLayout* rootLayout = qobject_cast<QVBoxLayout*>(popupKeyboard->layout());
-    rootLayout->setSpacing(10);
+    // Disable layout updates
+    rootLayout->setEnabled(false);
+    rootLayout->setSpacing(0);
 
-    for (int i = 0; i < additionalRows.size(); ++i) {
+    for (int i = 0; i < rowsCount; ++i) {
         QHBoxLayout* hRow = new QHBoxLayout();
-        // Set spacing between keys in this row (Horizontal)
-        hRow->setSpacing(10);
+        hRow->setContentsMargins(0, 0, 0, 0);
+        hRow->setSpacing(0);
 
         // Add every key in this row to the horizontal layout
         const QVector<VirtualKey*>& keys = additionalRows[i].keys;
         for (VirtualKey* k : keys) {
             k->setContentsMargins(0, 0, 0, 0);
-            k->setFixedSize(keySize->width(), keySize->height());
+            k->setFixedSize(*keySize);
+
             hRow->addWidget(k);
         }
 
-        // Add this horizontal row to the vertical root layout
         rootLayout->addLayout(hRow);
     }
 
-    popupKeyboard->setFixedSize(popupKeyboard->width(), popupKeyboard->height() * (1 + additionalRows.size()));
+    // Enable layout updates again
+    rootLayout->setEnabled(true);
+    // Increate popup's height
+    popupKeyboard->setFixedSize(popupKeyboard->width(), popupKeyboard->height() * (1 + rowsCount));
 };
 
 
